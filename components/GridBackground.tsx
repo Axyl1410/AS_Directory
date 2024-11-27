f4a1e356a9396c17be8006f54992d47d27f4544c
AlexNe <truonggiang190689@gmail.com>
2024-11-27 23:11:05 +0700

    enhance Hero component with content and search functionality; update GridBackground to accept children

--- components/GridBackground.tsx
+++ components/GridBackground.tsx
@@ -1,7 +1,14 @@
-export function GridBackground() {
+import React from "react";
+
+interface GridBackgroundProps {
+  children: React.ReactNode;
+}
+
+export function GridBackground({ children }: GridBackgroundProps) {
   return (
-    <div className="bg-grid-white/[0.2] relative flex h-[531px] w-full items-center justify-center bg-primary">
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-primary [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    <div className="relative flex h-[531px] w-full items-center justify-center bg-primary bg-grid-white/[0.2]">
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-primary [mask-image:radial-gradient(ellipse_at_center,transparent_70%,black)]"></div>
+      {children}
     </div>
   );
 }
