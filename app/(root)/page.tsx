1355d8d1019bed7e9fba98e1fc8c14eeeb71b3be
AlexNe <truonggiang190689@gmail.com>
2024-12-15 19:01:29 +0700

    refactor(home, card): enhance Post interface, improve layout structure, and update card styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,84 +1,102 @@
-// interface Post {
-//   _createdAt: Date;
-//   views: number;
-//   author: { _id: number; name: string };
-//   _id: number;
-//   description: string;
-//   image: string;
-//   category: string;
-//   title: string;
-// }
+interface Post {
+  _createdAt: Date;
+  views: number;
+  author: { _id: number; name: string };
+  _id: number;
+  description: string;
+  image: string;
+  category: string;
+  title: string;
+}
 
+import Card from "@/components/ui/card";
+import { LinkPreview } from "@/components/ui/link-preview";
+import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default function Home() {
   // {
   // searchParams,
   // }: {
   // searchParams: Promise<{ query?: string }>;
   // },
   // const query = (await searchParams).query;
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "John Doe" },
-  //     _id: 1,
-  //     description: "This is a description",
-  //     image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "John Doe" },
-  //     _id: 2,
-  //     description: "This is a description",
-  //     image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "John Doe" },
-  //     _id: 3,
-  //     description: "This is a description",
-  //     image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "John Doe" },
-  //     _id: 4,
-  //     description: "This is a description",
-  //     image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
-  //     category: "Robots",
-  //     title: "We Robots",
-  //   },
-  // ];
+  const posts: Post[] = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 2,
+      description: "This is a description",
+      image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 3,
+      description: "This is a description",
+      image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 4,
+      description: "This is a description",
+      image: "https://nguyentruonggiang.id.vn/image/img1.jfif",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <div className="px-5">
       <div className="container my-10 flex w-full grid-cols-[300px,1fr] flex-col-reverse gap-5 lg:grid">
-        <div className="top-6 z-10 flex h-full min-h-screen w-[300px] flex-col gap-4 lg:static">
-          <details open>
-            <summary className="cursor-pointer font-semibold">
-              Getting Started
-            </summary>
-            <Link href="/">
-              <p className="mt-2 w-fit rounded-md bg-linkShade p-2.5 text-sm text-link dark:bg-nav-dark">
-                Introduction
-              </p>
-            </Link>
-          </details>
+        <div className="top-6 z-10 flex h-full w-[300px] flex-col gap-4 lg:static">
+          {[
+            {
+              title: "Getting Started",
+              children: [
+                {
+                  title: "Introduction",
+                  link: "/",
+                },
+              ],
+            },
+          ].map((section) => (
+            <details key={section.title} open>
+              <summary className="cursor-pointer font-semibold">
+                {section.title}
+              </summary>
+              {section.children.map((child) => (
+                <Link key={child.link} href={child.link}>
+                  <p className="mt-2 w-fit rounded-md bg-linkShade p-2 text-sm text-link dark:bg-nav-dark">
+                    {child.title}
+                  </p>
+                </Link>
+              ))}
+            </details>
+          ))}
         </div>
-        <div className="text-[24px] font-medium text-text dark:text-text-dark lg:text-[46px]">
-          <p>
+
+        <div className="font-medium text-text dark:text-text-dark">
+          <p className="text-[24px] sm:text-[30px] lg:text-[46px]">
             Welcome to my Directory
             <span className="inline-block">
               <Image
@@ -91,6 +109,25 @@ export default async function Home() {
             </span>
             I&apos;m Alex and here I document our latest explorations.
           </p>
+
+          <div className="my-8 flex gap-2 text-sm">
+            <div className="flex w-fit items-center gap-1 rounded-md bg-linkShade p-2 text-sm text-link dark:bg-nav-dark">
+              <p>Gallery</p>
+              <ArrowUpRight size={16} />
+            </div>
+            <LinkPreview url="https://nguyentruonggiang.id.vn">
+              <div className="flex w-fit items-center rounded-md bg-linkShade p-2 text-sm text-link dark:bg-nav-dark">
+                <p>About me</p>
+                <ArrowUpRight size={16} />
+              </div>
+            </LinkPreview>
+          </div>
+
+          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {posts.map((post) => (
+              <Card key={post._id} {...post} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,5 @@
 import { formatDate } from "@/lib/utils";
+import { Eye } from "lucide-react";
 import SkeletonImage from "./skeleton-image";
 
 interface Author {
@@ -26,41 +27,24 @@ const Card = ({
   title,
 }: CardProps) => {
   return (
-    <div className="group flex flex-col gap-4 rounded-3xl border-4 border-b-8 border-r-8 border-black bg-white p-4 font-work-sans transition-colors hover:border-primary hover:bg-primary-100">
+    <div className="flex flex-col gap-4 rounded-3xl border-2 border-b-4 border-r-4 border-black bg-background p-4 font-work-sans text-text transition-colors dark:border-white dark:bg-background-dark dark:text-text-dark">
       <div className="flex w-full items-center justify-between">
-        <p className="rounded-full bg-primary-100 p-2.5 font-medium transition-colors group-hover:bg-white">
+        <p className="rounded-full py-2.5 text-sm font-medium transition-colors duration-300 ease-in">
           {formatDate(_createdAt)}
         </p>
         <div className="flex gap-1">
-          <svg
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M2.03601 12.322C1.967 12.1146 1.967 11.8904 2.03601 11.683C3.42301 7.51 7.36001 4.5 12 4.5C16.638 4.5 20.573 7.507 21.963 11.678C22.033 11.885 22.033 12.109 21.963 12.317C20.577 16.49 16.64 19.5 12 19.5C7.36201 19.5 3.42601 16.493 2.03601 12.322Z"
-              stroke="#EE2B69"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M15 12C15 12.7956 14.6839 13.5587 14.1213 14.1213C13.5587 14.6839 12.7956 15 12 15C11.2044 15 10.4413 14.6839 9.87868 14.1213C9.31607 13.5587 9 12.7956 9 12C9 11.2044 9.31607 10.4413 9.87868 9.87868C10.4413 9.31607 11.2044 9 12 9C12.7956 9 13.5587 9.31607 14.1213 9.87868C14.6839 10.4413 15 11.2044 15 12Z"
-              stroke="#EE2B69"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <p className="font-medium">{views}</p>
+          <Eye className="h-5 w-5 md:h-6 md:w-6" strokeWidth={1.5} />
+          <p className="text-sm font-medium">{views}</p>
         </div>
       </div>
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-2">
-          <p className="font-medium">{author.name}</p>
-          <p className="text-2xl font-semibold">{title}</p>
+          <p className="cursor-pointer text-sm font-medium hover:underline">
+            {author.name}
+          </p>
+          <p className="cursor-pointer text-xl font-semibold hover:underline md:text-2xl">
+            {title}
+          </p>
         </div>
         <SkeletonImage
           src={image}
@@ -69,15 +53,17 @@ const Card = ({
           className="aspect-square rounded-full object-cover"
         />
       </div>
-      <div>{description}</div>
+      <div className="text-sm">{description}</div>
       <SkeletonImage
         className="aspect-video rounded-lg object-cover"
         height="150px"
         src={image}
       />
       <div className="flex items-center justify-between">
-        <p className="font-medium">{category}</p>
-        <button className="rounded-full bg-black px-4 py-2 text-white">
+        <p className="cursor-pointer text-sm font-medium hover:underline">
+          {category}
+        </p>
+        <button className="rounded-full bg-black px-4 py-2 text-sm text-white">
           Details
         </button>
       </div>
