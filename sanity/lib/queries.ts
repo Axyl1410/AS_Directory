0734077b370e3121e0574cba287e60c357f1260a
AlexNe <truonggiang190689@gmail.com>
2024-12-29 13:37:42 +0700

    feat: enhance authentication flow with GitHub; add user creation and session management; improve user post display logic

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -39,6 +39,12 @@ export const BLOG_BY_AUTHOR_QUERY = defineQuery(
   `*[_type == "blog" && author -> _id == $id]${BLOG_PROJECTION}`,
 );
 
+export const VIEW_BLOG_QUERY = defineQuery(
+  `*[_type == "blog" && _id == $id]{
+    view
+  }`,
+);
+
 // Author queries
 export const ALL_AUTHORS_QUERY = defineQuery(
   `*[_type == "author" && !defined($search) || username match $search || name match $search]${AUTHOR_PROJECTION}`,
@@ -48,8 +54,14 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(
   `*[_type == "author" && _id == $id][0]${AUTHOR_PROJECTION}`,
 );
 
-export const VIEW_BLOG_QUERY = defineQuery(
-  `*[_type == "blog" && _id == $id]{
-    view
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
+  `*[_type == "author" && id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
   }`,
 );
